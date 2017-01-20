138f8771cda451c3fdeb4914750b86c2dbb09f34
mixxuss <mixxuss@gmail.com>
2017-01-20 17:36:13 +0300

    Added: api request               dict repo:issues_amount

--- github_trending.py
+++ github_trending.py
@@ -2,7 +2,6 @@ import requests
 import datetime
 
 
-# https://api.github.com/search/repositories?q=+created:%3E2017-01-13&sort=stars&order=desc&per_page=20
 API_RESOURCE = 'https://api.github.com/'
 PERIOD = str(datetime.date.today() - datetime.timedelta(days=7))
 
@@ -13,32 +12,16 @@ def get_trending_repositories(results_count):
     return repos.json()['items']
 
 
-def make_repos_list(repos_full_data):
-    repos_list = [repo['full_name'] for repo in repos_full_data]
-    return repos_list
-
-
-def get_open_issues_amount(repo_full_names_list):
-    issues_list = []
-    for repo_name in repo_full_names_list:
-        issues = requests.get(API_RESOURCE + 'repos/' + repo_name + '/issues').json()
-        issues_list.append(issues)
-    return issues_list
+def get_open_issues_amount(all_repos):
+    repos_issue = {}
+    for repo in all_repos:
+        repos_issue[repo['html_url']] = repo['open_issues']
+    return repos_issue
 
 
 if __name__ == '__main__':
     all_repos = get_trending_repositories(20)
-    # print(type(get_trending_repositories(20)))
-    # print(make_repos_list(all_repos))
-    all_opened_issues = make_repos_list(all_repos)
-    repos_issue = {}
-    for repo in all_repos:
-        repos_issue[repo['html_url']] = repo['open_issues']
-    print(repos_issue)
-    '''print(all_opened_issues)
-    for issues in all_opened_issues:
-        issues_amount = 0
-        for issue in issues:
-            issues_amount += 1
-        print(issues_amount)
-'''
+    issues_amount = get_open_issues_amount(all_repos)
+    print(issues_amount)
+    for repo, issue in issues_amount.items():
+        print('Repository %s have %d issues' % (repo, issue))
